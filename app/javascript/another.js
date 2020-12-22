a853530e0dd902092f85247f3f5a41d4d55f911c
jadedragon21 <74530173+jadedragon21@users.noreply.github.com>
2020-12-21 21:41:45 -0600

    2nd attempt
    webpack serve
    run ctrl+shift+D
    add port and go to town

--- app/javascript/another.js
+++ app/javascript/another.js
@@ -1,19 +1,16 @@
 function sayHello(){
     let header = document.querySelector("#greeting");
-    let hello = ("<h1> Get Served!</h1>");
+    let hello = ("<h1> Pancake Server Service Today</h1>");
     header.innerHTML = hello;
   }
   function bodyText(){
       let body= document.querySelector("#greeting2");
-      let body1 = ('<p>Lost in the Zone</p>');
+      let body1 = ('<p>What you wanted syrup with that?    </p>');
       body.innerHTML = body1;
   }
-  function raidList(){
-      let raid= document.querySelector("#greeting3");
-      let raid1= ('<body><p2>The Dog Jumps over the moon</p2></body>');
-  }
+ 
  
   module.exports = sayHello();
   module.exports = bodyText();
-  module.exports = raidList();
+  
  
